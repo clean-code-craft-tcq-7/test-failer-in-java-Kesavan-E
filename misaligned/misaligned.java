725a5554bdc16349f7a9a42d3e2728baf258a09f
Kesavan <116025852+Kesavan-E@users.noreply.github.com>
2022-11-30 22:10:58 +0530

    Update misaligned.java

--- misaligned/misaligned.java
+++ misaligned/misaligned.java
@@ -3,17 +3,18 @@ import java.util.Map;
 import java.util.*;
 
 public class Misaligned {
-    static int printColorMap() {
-        String majorColors[] = {"White", "Red", "Black", "Yellow", "Violet"};
-        String minorColors[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-        int i = 0, j = 0;
-        for(i = 0; i < 5; i++) {
-            for(j = 0; j < 5; j++) {
-                System.out.printf("%d | %s | %s\n", i * 5 + j, majorColors[i], minorColors[i]);
-            }
-        }
-        return i * j;
+    static String majorColors[] = {"White", "Red", "Black", "Yellow", "Violet"};
+    static String minorColors[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+    static String colorPairEntry(int majorColorIndex , int minorColorIndex ){
+        String index = String.valueOf(majorColorIndex*5 + minorColorIndex +1);
+        String str = new String(index+" | "+majorColors[majorColorIndex]+" | "+ minorColors[minorColorIndex]);
+        print(str);
+        return str;
     }
+    
+    static void  print(String colorPairEntryWithIndex){
+        System.out.println(colorPairEntryWithIndex);
+         
     public static void main(String[] args) { 
         int result = printColorMap();
         assert(result == 25);
